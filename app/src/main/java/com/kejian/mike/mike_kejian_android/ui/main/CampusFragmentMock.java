731458b6123cf5edd18da8bb3894fe57ff7497dd
oV0Vo <773977001@163.com>
2015-09-08 11:10:29 +0800

    添加了侧滑以及导航栏，侧滑和导航栏一起使用的时候还有些bug

---
+++ app/src/main/java/com/kejian/mike/mike_kejian_android/ui/main/CampusFragmentMock.java
@@ -0,0 +1,73 @@
+package com.kejian.mike.mike_kejian_android.ui.main;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kejian.mike.mike_kejian_android.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * to handle interaction events.
+ * Use the {@link CampusFragmentMock#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CampusFragmentMock extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CampusFragmentMock.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CampusFragmentMock newInstance(String param1, String param2) {
+        CampusFragmentMock fragment = new CampusFragmentMock();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public CampusFragmentMock() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_campus_fragment_mock, container, false);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
---
+++ app/src/main/java/com/kejian/mike/mike_kejian_android/ui/util/NeedRefinedAnnotation.java
@@ -0,0 +1,7 @@
+package com.kejian.mike.mike_kejian_android.ui.util;
+
+/**
+ * Created by violetMoon on 2015/9/8.
+ */
+public @interface NeedRefinedAnnotation {
+}
