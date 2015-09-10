7583f71fb15c9cff8fd26d241b9dea1940db1dcd
showjoy <hzh0895@126.com>
2015-09-10 18:24:44 +0800

    campus model

---
+++ app/src/main/java/model/campus/Post.java
@@ -0,0 +1,55 @@
+package model.campus;
+
+import java.util.ArrayList;
+
+/**
+ * Created by showjoy on 15/9/10.
+ */
+public class Post {
+    private int userId;
+    private String title;
+    private String content;
+    private int praise;
+    private ArrayList<Reply> replyList;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getPraise() {
+        return praise;
+    }
+
+    public void setPraise(int praise) {
+        this.praise = praise;
+    }
+
+    public ArrayList<Reply> getReplyList() {
+        return replyList;
+    }
+
+    public void setReplyList(ArrayList<Reply> replyList) {
+        this.replyList = replyList;
+    }
+
+}
