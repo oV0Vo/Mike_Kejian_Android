44a40c73c64e14decd74efebca41dcbb64018a83
zfy1696621890 <1696621890@qq.com>
2015-10-08 22:56:31 +0800

    change class name

---
+++ app/src/main/java/model/user/Global.java
@@ -0,0 +1,35 @@
+package model.user;
+
+import java.util.HashMap;
+
+/**
+ * Created by kisstheraik on 15/10/8.
+ */
+ public  class Global {
+
+
+    private static HashMap<String,Object> itemList=new HashMap<String, Object>();
+
+    private Global(){
+
+    }
+
+    public static Object getObjectByName(String name){
+
+        return itemList.get(name);
+
+    }
+
+    public static void addGlobalItem(String name,Object item){
+
+        itemList.put(name,item);
+
+
+    }
+
+    public static void deleteGlobalItemByName(String name){
+
+        itemList.remove(name);
+
+    }
+}
