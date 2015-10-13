379709ed75a2b8142bf44f91c7116d9414a8c448
hzh0895 <hzh0895@126.com>
2015-10-13 17:45:53 +0800

    post detail ui

--- app/src/main/java/com/kejian/mike/mike_kejian_android/ui/campus/PostDetailActivity.java
+++ app/src/main/java/com/kejian/mike/mike_kejian_android/ui/campus/PostDetailActivity.java
@@ -16,6 +16,9 @@ import android.widget.TextView;
 import com.kejian.mike.mike_kejian_android.R;
 import com.kejian.mike.mike_kejian_android.ui.user.UserBaseInfoOtherView;
 
+import bl.CampusBLService;
+import model.campus.Post;
+import model.campus.Reply;
 import model.user.Invitee;
 
 /**
@@ -26,6 +29,8 @@ public class PostDetailActivity extends AppCompatActivity {
     ActionBar actionBar;
     private TextView author;
     private Context context;
+    private Post post;
+    private String postId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,7 @@ public class PostDetailActivity extends AppCompatActivity {
         actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
+        iniData();
         iniView();
     }
 
@@ -51,7 +57,8 @@ public class PostDetailActivity extends AppCompatActivity {
     }
 
     private void iniData() {
-
+        postId = getIntent().getStringExtra("postId");
+        post = CampusBLService.getPostDetail("test");
     }
 
     private void iniView(){
@@ -61,13 +68,38 @@ public class PostDetailActivity extends AppCompatActivity {
         author.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent();
+                Intent intent = new Intent();
                 intent.setClass(context, UserBaseInfoOtherView.class);
                 startActivity(intent);
             }
         });
-        for(int i=0; i<10; i++) {
+        TextView title = (TextView)findViewById(R.id.title_view);
+        title.setText(post.getTitle());
+        TextView content = (TextView)findViewById(R.id.content_view);
+        content.setText(post.getContent());
+        TextView author = (TextView)findViewById(R.id.author_name_view);
+        author.setText(post.getAuthorName());
+        TextView date = (TextView)findViewById(R.id.date_view);
+        date.setText(post.getDate().toString());
+        TextView viewNum = (TextView)findViewById(R.id.view_num_view);
+        viewNum.setText(Integer.toString(post.getPraise()));
+        TextView commentNum = (TextView)findViewById(R.id.comment_num_view);
+        commentNum.setText(Integer.toString(post.getReplyList().size()));
+        TextView replyNumView = (TextView)findViewById(R.id.reply_num_view);
+        replyNumView.setText("共(" + post.getReplyList().size() + ")条");
+
+        for(Reply loopReply: post.getReplyList()) {
             View replyView = layoutInflater.inflate(R.layout.layout_single_reply, null);
+            TextView replyContent = (TextView) replyView.findViewById(R.id.single_reply_content_view);
+            replyContent.setText(loopReply.getContent());
+            TextView replyAuthor = (TextView) replyView.findViewById(R.id.author_name_view);
+            replyAuthor.setText(loopReply.getAuthorName());
+            TextView replyDate = (TextView) replyView.findViewById(R.id.date_view);
+            replyDate.setText(loopReply.getDate().toString());
+            TextView replyViewNum = (TextView) replyView.findViewById(R.id.view_num_view);
+            replyViewNum.setText(Integer.toString(loopReply.getPraise()));
+            TextView replyCommentNum = (TextView) replyView.findViewById(R.id.comment_num_view);
+            replyCommentNum.setText(Integer.toString(loopReply.getSubReplyList().size()));
             reply_view.addView(replyView);
         }
 
