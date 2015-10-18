b231ecd7e601116e6188b3a7c8bc3cbe622e959c
DengrongGuan12 <1031212913@qq.com>
2015-10-18 10:28:15 +0800

    add icon_url

--- app/src/main/java/com/kejian/mike/mike_kejian_android/ui/main/SearchableActivity.java
+++ app/src/main/java/com/kejian/mike/mike_kejian_android/ui/main/SearchableActivity.java
@@ -1,5 +1,8 @@
 package com.kejian.mike.mike_kejian_android.ui.main;
 
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,12 +10,22 @@ import android.view.MenuItem;
 
 import com.kejian.mike.mike_kejian_android.R;
 
-public class SearchableActivity extends AppCompatActivity {
+public class SearchableActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchable);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+    private void doMySearch(String query){
+
     }
 
     @Override
