8d08f88dc8826c43697dad5e3dc12e948d1ab987
DengrongGuan12 <1031212913@qq.com>
2015-10-24 17:44:43 +0800

    搜索课程和帖子 添加网络

--- app/src/main/java/model/helper/SearchType.java
+++ app/src/main/java/model/helper/SearchType.java
@@ -4,7 +4,7 @@ package model.helper;
  * Created by I322233 on 10/24/2015.
  */
 public interface SearchType {
-    public static final int all = 2;
-    public static final int student = 0;
+    public static final int invite = 2;
+    public static final int addAssistant = 0;
     public static final int teacher = 1;
 }
