861a972329e40a00ae5c3f70d7d8a9f477f829c0
zfy1696621890 <1696621890@qq.com>
2015-10-28 13:14:21 +0800

    change class name

--- app/src/main/java/com/kejian/mike/mike_kejian_android/ui/campus/ReplyAdapter.java
+++ app/src/main/java/com/kejian/mike/mike_kejian_android/ui/campus/ReplyAdapter.java
@@ -14,6 +14,7 @@ import com.kejian.mike.mike_kejian_android.R;
 
 
 import net.picture.DownloadPicture;
+import net.picture.MessagePrint;
 
 import java.util.List;
 
@@ -61,6 +62,7 @@ public class ReplyAdapter extends ArrayAdapter<Reply>{
         }
 
         final Reply reply = getItem(position);
+        MessagePrint.print("reply url:"+reply.getUserIconUrl());
         DownloadPicture d=new DownloadPicture(getContext(),replyViewHolder.reply_user_icon, reply.getUserIconUrl(), reply.getUserIconUrl());
 
 
